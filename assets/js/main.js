068bd0b87bed29dd559d9d2af9ac9fb529521c82
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-07-29 09:20:19 -0300

    feat: adição do recurso de expandir menu para telas menores e botão de conversa pelo whatsapp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
 const cookieBanner = document.getElementById("cookie-banner");
 const acceptCookies = document.getElementById("accept-cookies");
 const rejectCookies = document.getElementById("reject-cookies");
+const menuToggle = document.getElementById("menu-toggle");
+const mainMenu = document.getElementById("main-menu");
+const menuLinks = document.querySelectorAll("#main-menu a");
 
+//função de abertura do menu em telas menores
+menuToggle.addEventListener("click", () => {
+  mainMenu.classList.toggle("active");
+
+  const expanded = menuToggle.getAttribute("aria-expanded") === "true";
+
+  menuToggle.setAttribute("aria-expanded", !expanded);
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mainMenu.classList.remove("active");
+
+    menuToggle.setAttribute("aria-expanded", "false");
+  });
+});
+
+//função para fechar banner de cookies
 if (acceptCookies && rejectCookies && cookieBanner) {
   acceptCookies.addEventListener("click", () => {
     cookieBanner.style.display = "none";
